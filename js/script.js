9654c5ea10393daae9b68bf73f13e0a4b63e515d
Stephania Mwikali <steph.mwikali@gmail.com>
2019-07-13 16:59:46 +0300

    Add arrays for female and male names

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 function calculateBirthday() {
   var form = document.forms["myForm"]
   var dd = form["date"].value;
-
   var mm = form["month"].value;
-
   var cc = form["century"].value;
-
   var yy = form["year"].value;
-  alert(dd,mm,cc,yy)
   alert("You were born on"+dd+"/"+mm+"/"+cc+yy);
   var gender=form["gender"].value;
-  alert(gender);
+  dayOfTheWeek(dd,mm,cc,yy);
+  var result = dayOfTheWeek(dd,mm,cc,yy);
+  alert(result);
 }
-
-function getAkanName(month, century) {
-
+function dayOfTheWeek(dd,mm,cc,yy) {
+  return (((cc/4)-2*cc-1)+(5*yy/4)+(26*(mm+1)/10)+dd)%7;
 }
+var maleNames=[Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kwame]
+var femaleNames=[Akosua,Adwoa,Abenaa,Akua,Yaa,Afua,Ama]
