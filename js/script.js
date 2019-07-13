8f92fc8f086d24c98a64023ad282c7b4d16416c4
Stephania Mwikali <steph.mwikali@gmail.com>
2019-07-13 21:16:23 +0300

    Add result to the html file after calculation

--- js/script.js
+++ js/script.js
@@ -1,37 +1,3 @@
-function calculateBirthday() {
-  var valid=true;
-  var form = document.forms["myForm"];
-  var dd = form["date"].value;
-  var mm = form["month"].value;
-  var cc = form["century"].value;
-  var yy = form["year"].value;
-  var gender=form["gender"].value;
-
-  if (dd<=0 || dd>31){
-    alert ("Invalid date. Enter valid date.");
-    valid=false;
-  }
-  if (mm<=0 || mm>12){
-    alert("Invalid month. Enter valid month");
-    valid=false;
-  }
-
-  if (valid) {
-    var result = dayOfTheWeek(dd,mm,cc,yy);
-    result = result.toFixed(0);
-    if (gender==='male') {
-      alert (maleNames[result]);
-    }
-    else{
-      alert(femaleNames[result]);
-    }
-  }
-
-
-}
-function dayOfTheWeek(dd,mm,cc,yy) {
-  return (((cc/4)-2*cc-1)+(5*yy/4)+(26*(mm+1)/10)+dd)%7;
-}
 var maleNames=[
   'Kwasi',
   'Kwadwo',
@@ -50,3 +16,39 @@ var femaleNames=[
   'Afua',
   'Ama',
 ]
+
+function dayOfTheWeek(dd,mm,cc,yy) {
+  return (((cc/4)-2*cc-1)+(5*yy/4)+(26*(mm+1)/10)+dd)%7;
+}
+
+function calculateBirthday() {
+  var valid=true;
+  var form = document.forms["myForm"];
+  var dd = form["date"].value;
+  var mm = form["month"].value;
+  var cc = form["century"].value;
+  var yy = form["year"].value;
+  var gender=form["gender"].value;
+
+  if (dd <= 0 || dd > 31){
+    alert ("Invalid date. Enter valid date.");
+    valid=false;
+  }
+  if (mm <= 0 || mm > 12){
+    alert("Invalid month. Enter valid month");
+    valid=false;
+  }
+
+  if (valid) {
+    var result = dayOfTheWeek(dd,mm,cc,yy);
+    result = result.toFixed(0);
+    if (gender==='male') {
+      alert ("Your Akan name is "+maleNames[result]);
+      document.getElementById("akan").innerHTML="Akan name is"+maleNames[result]
+    }
+    else{
+      alert("You Akan name is "+femaleNames[result]);
+      document.getElementById("akan").innerHTML="Akan name is"+femaleNames[result]
+    }
+  }
+}
